755c8a193e2630ef3396ddc3afcdcd84c37b7d03
Rafael Mejia <devrafx@gmail.com>
2019-11-29 23:21:23 -0400

    feat: add loading until the data from the server arrives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import {faAngular} from '@fortawesome/free-brands-svg-icons';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
+import {MyGitService} from './provider/my-git.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('isLoaded', [
+      state('loading', style({
+        top: 0
+      })),
+      state('stop', style({
+        top: '-2000px'
+      })),
+      transition('loading <=> stop', [
+        animate('1s')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'cv';
+  public icon = faAngular;
+  constructor(public api: MyGitService) {}
+
 }
