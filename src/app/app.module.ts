11a4d622259f262920193f86040474942eccdee4
Rafael Mejia <devrafx@gmail.com>
2019-11-29 23:22:17 -0400

    feat(modules): add modules to enable all the required functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
+import {NgmaterialModule} from './ngmaterial/ngmaterial.module';
+
+import { MyGitService } from './provider/my-git.service';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { CvBodyComponent } from './components/curriculum/cv-body/cv-body.component';
+import { CvAsideComponent } from './components/curriculum/cv-aside/cv-aside.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurriculumComponent,
+    CvBodyComponent,
+    CvAsideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgmaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MyGitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
