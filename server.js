f58d9c27528e2c53e37a0440f6d8ad5fcb064640
Rafael Mejia <devrafx@gmail.com>
2019-11-30 11:51:38 -0400

    fix: name generate wasn't  configured pre-deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/normal-resume'))
+app.use(express.static(__dirname + '/dist/cv'))
    .get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/normal-resume/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cv/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
