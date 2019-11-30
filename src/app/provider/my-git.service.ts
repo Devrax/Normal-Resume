7c6449982e09dcb5579552f5aae48b301d478b00
Rafael Mejia <devrafx@gmail.com>
2019-11-30 13:33:34 -0400

    feat: add a error handler and displayed through sweet alert

--- src/app/provider/my-git.service.ts
+++ src/app/provider/my-git.service.ts
@@ -1,40 +1,45 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 import {IUser, IRepo} from '../models/interfaces.types';
+import Swal from 'sweetalert2';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyGitService {
-  //the loading uses this boolean directly
+  // the loading uses this boolean directly
   public mainLoading = false;
   public counter = 0;
 
   private baseUrl = 'https://api.github.com';
 
-  //You must to specify your user in order to located your info
+  // You must to specify your user in order to located your info
   private who = 'devrax';
 
   constructor(public http: HttpClient) { }
 
   /**
    * This function is the one who makes the http get requets
-   * @param endpoint
-   * @param params
    */
   httpGet(endpoint: string, params?: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${endpoint}${params ? params : ''}`);
+    return this.http.get(`${this.baseUrl}/${endpoint}${params ? params : ''}`).pipe(
+      catchError(() => {
+        this.alert("Error", 'I cannot established a communication with Github\'s api', 'error');
+        return throwError("Fatal, no hay conexion a github");
+      })
+    )
   }
 
-  //this function is responsable to prepare the endpoint to get the user and returns that data
+  // this function is responsable to prepare the endpoint to get the user and returns that data
   getUser(): Observable<IUser> {
     return this.httpGet('users', `/${this.who}`);
   }
 
-  //this function is responsable to prepare the endpoint to get the user's repository and returns that data
+  // this function is responsable to prepare the endpoint to get the user's repository and returns that data
   getUserRepos(): Observable<IRepo[]> {
     return this.httpGet('users', `/${this.who}/repos`);
   }
@@ -49,6 +54,15 @@ export class MyGitService {
     if(this.counter === 2) {
       this.mainLoading = true;
     }
+  }
+
+  //Custom alert
+  alert(title: string, msg: string, type) {
+    Swal.fire(
+      title,
+      msg,
+      type
+    );
   }
 
 }
